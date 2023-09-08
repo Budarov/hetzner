9ba6184c0a1d64c0aff81e0d82b14c88baa7a001
Budarov <budarov@gmail.com>
2023-09-08 15:30:46 +0300

    Fix

--- hetzner/wol.py
+++ hetzner/wol.py
@@ -1,11 +1,11 @@
-
-#__all__ = ['WOL']
+#__all__ = ['wol']
 
 
 class WOL(object):
     def __init__(self, server):
         self.conn = server.conn
+        self.server = server
 
     def send_wol(self):
-        result = self.conn.put('/wol/{0}'.format(str(self.number)), None)
+        result = self.conn.put('/wol/{0}'.format(str(self.server.number)), None)
         return result
